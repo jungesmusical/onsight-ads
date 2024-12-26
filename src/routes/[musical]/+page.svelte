52bcc995c2e5611f7e89c5c86cc8491e9bf89618
Finn Nils Maybauer <hi@finnmaybauer.me>
2024-12-26 23:52:14 +0100

    feat: show data depending on timestamp

---
+++ src/routes/[musical]/+page.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import type { ShowData, SingleShow } from '$lib/ShowData';
+
+  function getNextShows( shows: ShowData['shows'], currentDate = new Date()): SingleShow[] {
+    const nextShows: SingleShow[] = [];
+
+    shows.forEach((show) => {
+      const start = new Date(show.timestamp)
+      start.setTime(start.getTime() - 1000 * 60 * 60 * 1.5); // 1.5 hours before show start
+
+      const end = new Date(show.timestamp)
+      end.setTime(end.getTime() + 1000 * 60 * 60 * 3); // 4 hours after show start
+
+      if (start < currentDate && currentDate < end) {
+        nextShows.push(show);
+      }
+    });
+
+    return nextShows;
+  }
+
+  let { data }: { data: PageData } = $props();
+	const { showData } = data;
+
+  const TEST_DATE = new Date('2025-03-21 20:00:00');
+
+  let currentTime = $state(new Date(TEST_DATE));
+
+  setInterval(() => {
+    currentTime = new Date(TEST_DATE);
+  }, 1000 * 60);
+
+  let nextShowsLive = $derived(getNextShows(showData.shows, currentTime));
+</script>
+
+<h1>{showData.common.title}</h1>
+
+<p>{showData.common.venue.name} in {showData.common.venue.city}</p>
+
+
+{#each nextShowsLive as show}
+  {show.timestamp}
+
+  <p>Es spielt {show.companies.join(', ')}</p>
+
+{/each}
+
+{#if nextShowsLive.length === 0}
+  <p>No shows currently live</p>
+{/if}
+
+
+
+<p>{currentTime}</p>
