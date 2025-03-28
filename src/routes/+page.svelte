fe2e2407ce9dd55cc3b333c84b99eb19cf8919c4
Finn Nils Maybauer <hi@finnmaybauer.me>
2025-03-28 15:25:13 +0100

    feat: add disclaimer section to show data and update navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,5 @@
 	<li><a href="cast">Cast</a></li>
 	<li><a href="orchester">Orchester</a></li>
 	<li><a href="crew">Crew</a></li>
+	<li><a href="disclaimer">Disclaimer</a></li>
 </ul>
---
+++ src/routes/disclaimer/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	let { data }: { data: PageData } = $props();
+	const { showData } = data;
+</script>
+
+<style>
+  p, h2 {
+    text-wrap: balance;
+    line-height: 140%;
+    margin-bottom: 1.5em;
+  }
+</style>
+
+<h2>Hinweis</h2>
+
+{#if showData.common.disclaimer}
+
+{#each showData.common.disclaimer.split("\n") as block}
+
+{#if block !== ""}
+<p class="fs-l ta-center">{block}</p>
+{/if}
+
+
+{/each}
+
+{/if}
+
