5044916bddabdc26e3853c30c8c3242bed6aec2b
Finn Nils Maybauer <hi@finnmaybauer.me>
2026-01-05 07:53:12 +0100

    feat: add layouts

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -5,10 +5,13 @@
 	const showData = $derived(data.showData);
 
 	const gallery = $derived(showData.common.gallery[0]);
+
+  let counter = 0;
 </script>
 
 <div class="gallery-grid">
 	{#each gallery.images as image}
+    {#if counter++ < 2}
 		<div class="gallery-grid__item">
 			<picture>
 				{#each image.source as source}
@@ -22,6 +25,7 @@
 				/>
 			</picture>
 		</div>
+    {/if}
 	{/each}
 </div>
 
@@ -32,18 +36,103 @@
 	.gallery-grid {
 		display: grid;
 		grid-template-columns: repeat(3, minmax(0, 1fr));
-		grid-template-rows: repeat(5, minmax(0, 1fr));
+		grid-template-rows: repeat(6, minmax(0, 1fr));
 		gap: var(--xxs);
 		padding: var(--xxs);
-		max-height: calc(100vh - 2 * var(--xxs));
+		height: calc(100vh - 2 * var(--xxs));
 
-		&:has(:nth-child(10)) {
+		&:has(:nth-child(10)):not(:has(:nth-child(11))) {
+			grid-template-rows: repeat(5, minmax(0, 1fr));
+
 			grid-template-areas:
 				'gallery_item_1 gallery_item_1 gallery_item_2'
 				'gallery_item_3 gallery_item_4 gallery_item_4'
 				'gallery_item_5 gallery_item_5 gallery_item_6'
 				'gallery_item_7 gallery_item_8 gallery_item_8'
 				'gallery_item_9 gallery_item_9 gallery_item_10';
+		}
+
+		&:has(:nth-child(9)):not(:has(:nth-child(10))) {
+			grid-template-rows: repeat(5, minmax(0, 1fr));
+
+			grid-template-areas:
+				'gallery_item_1 gallery_item_1 gallery_item_2'
+				'gallery_item_3 gallery_item_4 gallery_item_4'
+				'gallery_item_5 gallery_item_5 gallery_item_6'
+				'gallery_item_7 gallery_item_8 gallery_item_8'
+				'gallery_item_9 gallery_item_9 gallery_item_9';
+		}
+
+		&:has(:nth-child(8)):not(:has(:nth-child(9))) {
+			grid-template-areas:
+				'gallery_item_1 gallery_item_1 gallery_item_2'
+				'gallery_item_3 gallery_item_3 gallery_item_2'
+				'gallery_item_4 gallery_item_4 gallery_item_5'
+				'gallery_item_4 gallery_item_4 gallery_item_6'
+				'gallery_item_7 gallery_item_7 gallery_item_8'
+				'gallery_item_7 gallery_item_7 gallery_item_8';
+		}
+
+    &:has(:nth-child(7)):not(:has(:nth-child(8))) {
+		  grid-template-columns: repeat(4, minmax(0, 1fr));
+			grid-template-areas:
+        'gallery_item_1 gallery_item_1 gallery_item_2 gallery_item_2'
+        'gallery_item_3 gallery_item_3 gallery_item_2 gallery_item_2'
+        'gallery_item_3 gallery_item_3 gallery_item_4 gallery_item_4'
+        'gallery_item_5 gallery_item_5 gallery_item_4 gallery_item_4'
+        'gallery_item_6 gallery_item_6 gallery_item_4 gallery_item_4'
+        'gallery_item_6 gallery_item_6 gallery_item_7 gallery_item_7';
+		}
+
+    &:has(:nth-child(6)):not(:has(:nth-child(7))) {
+		  grid-template-rows: repeat(3, minmax(0, 1fr));
+
+			grid-template-areas:
+				'gallery_item_1 gallery_item_1 gallery_item_2'
+				'gallery_item_3 gallery_item_4 gallery_item_4'
+				'gallery_item_5 gallery_item_5 gallery_item_6';
+		}
+
+    &:has(:nth-child(5)):not(:has(:nth-child(6))) {
+		  grid-template-rows: repeat(3, minmax(0, 1fr));
+
+			grid-template-areas:
+				'gallery_item_1 gallery_item_1 gallery_item_1'
+				'gallery_item_2 gallery_item_3 gallery_item_3'
+				'gallery_item_4 gallery_item_4 gallery_item_5';
+		}
+
+    &:has(:nth-child(4)):not(:has(:nth-child(5))) {
+		  grid-template-rows: repeat(3, minmax(0, 1fr));
+
+			grid-template-areas:
+				'gallery_item_1 gallery_item_1 gallery_item_1'
+				'gallery_item_2 gallery_item_3 gallery_item_3'
+				'gallery_item_4 gallery_item_4 gallery_item_4';
+		}
+
+    &:has(:nth-child(3)):not(:has(:nth-child(4))) {
+		  grid-template-rows: repeat(3, minmax(0, 1fr));
+
+			grid-template-areas:
+				'gallery_item_1 gallery_item_1 gallery_item_1'
+				'gallery_item_2 gallery_item_2 gallery_item_2'
+				'gallery_item_3 gallery_item_3 gallery_item_3';
+		}
+
+    &:has(:nth-child(2)):not(:has(:nth-child(3))) {
+		  grid-template-rows: repeat(2, minmax(0, 1fr));
+
+			grid-template-areas:
+				'gallery_item_1 gallery_item_1 gallery_item_1'
+				'gallery_item_2 gallery_item_2 gallery_item_2';
+		}
+
+    &:has(:nth-child(1)):not(:has(:nth-child(2))) {
+		  grid-template-rows: repeat(1, minmax(0, 1fr));
+
+			grid-template-areas:
+				'gallery_item_1 gallery_item_1 gallery_item_1';
 		}
 
 		&__item {
