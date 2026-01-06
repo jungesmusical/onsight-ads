fb9100c87470b3de0d46d7b2b7171c36e6c9a306
Finn Nils Maybauer <hi@finnmaybauer.me>
2026-01-06 13:04:06 +0100

    feat: enhance gallery layout with header and responsive styles

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -5,13 +5,14 @@
 	const showData = $derived(data.showData);
 
 	const gallery = $derived(showData.common.gallery[0]);
-
-  let counter = 0;
 </script>
+
+<header class="gallery-heading">
+	<h1>{gallery.title} <span>{gallery.year}</span></h1>
+</header>
 
 <div class="gallery-grid">
 	{#each gallery.images as image}
-    {#if counter++ < 2}
 		<div class="gallery-grid__item">
 			<picture>
 				{#each image.source as source}
@@ -25,7 +26,6 @@
 				/>
 			</picture>
 		</div>
-    {/if}
 	{/each}
 </div>
 
@@ -40,8 +40,9 @@
 		gap: var(--xxs);
 		padding: var(--xxs);
 		height: calc(100vh - 2 * var(--xxs));
+		position: relative;
 
-		&:has(:nth-child(10)):not(:has(:nth-child(11))) {
+		&:has(> :nth-child(10)):not(:has(> :nth-child(11))) {
 			grid-template-rows: repeat(5, minmax(0, 1fr));
 
 			grid-template-areas:
@@ -52,7 +53,7 @@
 				'gallery_item_9 gallery_item_9 gallery_item_10';
 		}
 
-		&:has(:nth-child(9)):not(:has(:nth-child(10))) {
+		&:has(> :nth-child(9)):not(:has(> :nth-child(10))) {
 			grid-template-rows: repeat(5, minmax(0, 1fr));
 
 			grid-template-areas:
@@ -63,7 +64,7 @@
 				'gallery_item_9 gallery_item_9 gallery_item_9';
 		}
 
-		&:has(:nth-child(8)):not(:has(:nth-child(9))) {
+		&:has(> :nth-child(8)):not(:has(> :nth-child(9))) {
 			grid-template-areas:
 				'gallery_item_1 gallery_item_1 gallery_item_2'
 				'gallery_item_3 gallery_item_3 gallery_item_2'
@@ -73,19 +74,19 @@
 				'gallery_item_7 gallery_item_7 gallery_item_8';
 		}
 
-    &:has(:nth-child(7)):not(:has(:nth-child(8))) {
-		  grid-template-columns: repeat(4, minmax(0, 1fr));
+		&:has(> :nth-child(7)):not(:has(> :nth-child(8))) {
+			grid-template-columns: repeat(4, minmax(0, 1fr));
 			grid-template-areas:
-        'gallery_item_1 gallery_item_1 gallery_item_2 gallery_item_2'
-        'gallery_item_3 gallery_item_3 gallery_item_2 gallery_item_2'
-        'gallery_item_3 gallery_item_3 gallery_item_4 gallery_item_4'
-        'gallery_item_5 gallery_item_5 gallery_item_4 gallery_item_4'
-        'gallery_item_6 gallery_item_6 gallery_item_4 gallery_item_4'
-        'gallery_item_6 gallery_item_6 gallery_item_7 gallery_item_7';
+				'gallery_item_1 gallery_item_1 gallery_item_2 gallery_item_2'
+				'gallery_item_3 gallery_item_3 gallery_item_2 gallery_item_2'
+				'gallery_item_3 gallery_item_3 gallery_item_4 gallery_item_4'
+				'gallery_item_5 gallery_item_5 gallery_item_4 gallery_item_4'
+				'gallery_item_6 gallery_item_6 gallery_item_4 gallery_item_4'
+				'gallery_item_6 gallery_item_6 gallery_item_7 gallery_item_7';
 		}
 
-    &:has(:nth-child(6)):not(:has(:nth-child(7))) {
-		  grid-template-rows: repeat(3, minmax(0, 1fr));
+		&:has(> :nth-child(6)):not(:has(> :nth-child(7))) {
+			grid-template-rows: repeat(3, minmax(0, 1fr));
 
 			grid-template-areas:
 				'gallery_item_1 gallery_item_1 gallery_item_2'
@@ -93,8 +94,8 @@
 				'gallery_item_5 gallery_item_5 gallery_item_6';
 		}
 
-    &:has(:nth-child(5)):not(:has(:nth-child(6))) {
-		  grid-template-rows: repeat(3, minmax(0, 1fr));
+		&:has(> :nth-child(5)):not(:has(> :nth-child(6))) {
+			grid-template-rows: repeat(3, minmax(0, 1fr));
 
 			grid-template-areas:
 				'gallery_item_1 gallery_item_1 gallery_item_1'
@@ -102,8 +103,8 @@
 				'gallery_item_4 gallery_item_4 gallery_item_5';
 		}
 
-    &:has(:nth-child(4)):not(:has(:nth-child(5))) {
-		  grid-template-rows: repeat(3, minmax(0, 1fr));
+		&:has(> :nth-child(4)):not(:has(> :nth-child(5))) {
+			grid-template-rows: repeat(3, minmax(0, 1fr));
 
 			grid-template-areas:
 				'gallery_item_1 gallery_item_1 gallery_item_1'
@@ -111,28 +112,30 @@
 				'gallery_item_4 gallery_item_4 gallery_item_4';
 		}
 
-    &:has(:nth-child(3)):not(:has(:nth-child(4))) {
-		  grid-template-rows: repeat(3, minmax(0, 1fr));
+		&:has(> :nth-child(3)):not(:has(> :nth-child(4))) {
+			grid-template-rows: repeat(3, minmax(0, 1fr));
+			grid-template-columns: minmax(0, 1fr);
 
 			grid-template-areas:
-				'gallery_item_1 gallery_item_1 gallery_item_1'
-				'gallery_item_2 gallery_item_2 gallery_item_2'
-				'gallery_item_3 gallery_item_3 gallery_item_3';
+				'gallery_item_1'
+				'gallery_item_2'
+				'gallery_item_3';
 		}
 
-    &:has(:nth-child(2)):not(:has(:nth-child(3))) {
-		  grid-template-rows: repeat(2, minmax(0, 1fr));
+		&:has(> :nth-child(2)):not(:has(> :nth-child(3))) {
+			grid-template-rows: repeat(2, minmax(0, 1fr));
+			grid-template-columns: minmax(0, 1fr);
 
 			grid-template-areas:
-				'gallery_item_1 gallery_item_1 gallery_item_1'
-				'gallery_item_2 gallery_item_2 gallery_item_2';
+				'gallery_item_1'
+				'gallery_item_2';
 		}
 
-    &:has(:nth-child(1)):not(:has(:nth-child(2))) {
-		  grid-template-rows: repeat(1, minmax(0, 1fr));
+		&:has(> :nth-child(1)):not(:has(> :nth-child(2))) {
+			grid-template-rows: minmax(0, 1fr);
+			grid-template-columns: minmax(0, 1fr);
 
-			grid-template-areas:
-				'gallery_item_1 gallery_item_1 gallery_item_1';
+			grid-template-areas: 'gallery_item_1';
 		}
 
 		&__item {
@@ -146,6 +149,36 @@
 				width: 100%;
 				height: 100%;
 				object-fit: cover;
+			}
+		}
+	}
+
+	.gallery-heading {
+		--margin-overlap: 6rem;
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		padding: var(--xxl);
+		padding-top: calc(var(--xxl) + var(--margin-overlap));
+		color: white;
+		z-index: 1;
+		background: linear-gradient(
+			180deg,
+			rgba(0, 0, 0, 0) 0,
+			rgba(0, 0, 0, 0.66) var(--margin-overlap)
+		);
+
+		h1 {
+			font-size: var(--fs-l);
+			font-weight: var(--fw-strong);
+			text-align: start;
+			margin: 0;
+			line-height: 1;
+			span {
+				font-weight: var(--fw-regular);
+				display: inline-block;
+				margin-inline-start: 0.5ch;
 			}
 		}
 	}
